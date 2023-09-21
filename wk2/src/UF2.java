18dbecfbe6be97ef85ab3ded73686b0e2fa549e3
diepxuanlinh <22021193@vnu.edu.vn>
2023-09-21 13:00:01 +0700

    w2

---
+++ wk2/src/UF2.java
@@ -0,0 +1,37 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.UF;
+
+public class UF2 {
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+
+        int components = N; // Số lượng thành phần liên thông ban đầu là N
+        int cnt = 0;
+        boolean fail = true;
+
+        while (!StdIn.isEmpty()) {
+
+
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            cnt++;
+
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                components--;
+
+
+                if (components == 1) {
+                    StdOut.println(cnt);
+                    fail = false;
+                    return;
+                }
+            }
+
+        }
+
+       if(fail) StdOut.println("FAILED");
+    }
+}
